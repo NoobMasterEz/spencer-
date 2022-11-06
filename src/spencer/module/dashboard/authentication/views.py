3f95cd5f9524358ae15be141fa5747638fba6027
noobmaster <b13m123kiss@gmail.com>
2022-11-06 23:57:16 +0700

    [code update]: add get_redirect_url

--- src/spencer/module/dashboard/authentication/views.py
+++ src/spencer/module/dashboard/authentication/views.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
+from django.utils.http import url_has_allowed_host_and_scheme
 
 
 class LoginForm(AuthenticationForm):
@@ -11,3 +12,16 @@ class LoginForm(AuthenticationForm):
 class LoginViewSet(LoginView):
     authentication_form = LoginForm
     template_name = 'dashboard/accounts/login.html'
+
+    def get_redirect_url(self):
+        """Return the user-originating redirect URL if it's safe."""
+        redirect_to = self.request.POST.get(
+            self.redirect_field_name,
+            self.request.GET.get(self.redirect_field_name, '')
+        )
+        url_is_safe = url_has_allowed_host_and_scheme(
+            url=redirect_to,
+            allowed_hosts=self.get_success_url_allowed_hosts(),
+            require_https=self.request.is_secure(),
+        )
+        return redirect_to if url_is_safe else ''
